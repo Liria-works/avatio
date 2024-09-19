8dee41db5f46fe08356a45b94ae4beb4072a1088
Liry24 <liry241999@gmail.com>
2024-09-20 08:15:04 +0900

    dev

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -10,30 +10,3 @@ export const supabase = await createClient(
 	import.meta.env.PUBLIC_SUPABASE_URL,
 	import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
 );
-
-export async function supabaseSession(cookies) {
-	const accessToken = cookies.get("sb-access-token");
-	const refreshToken = cookies.get("sb-refresh-token");
-
-	if (accessToken && refreshToken) {
-		const { data, error } = await supabase.auth.setSession({
-			refresh_token: refreshToken.value,
-			access_token: accessToken.value,
-		});
-
-		if (error) {
-			cookies.delete("sb-access-token", {
-				path: "/",
-			});
-			cookies.delete("sb-refresh-token", {
-				path: "/",
-			});
-
-			return null;
-		}
-
-		if (data) {
-			return data;
-		}
-	}
-}
