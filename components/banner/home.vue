a2012ac2fde067ec8349a3dd68d3ba04802402ee
Liry24 <47878070+Liry24@users.noreply.github.com>
2024-12-04 17:18:38 +0900

    Update home.vue

--- components/banner/home.vue
+++ components/banner/home.vue
@@ -1,12 +1,12 @@
 <template>
     <div
-        class="w-full h-64 p-12 flex flex-col justify-between rounded-xl outline outline-2 outline-neutral-300 dark:outline-neutral-600 ring-4 ring-inset ring-neutral-50 dark:ring-neutral-800 bg-neutral-200 dark:bg-neutral-700"
+        class="w-full min-h-64 p-12 gap-4 flex flex-col justify-between rounded-xl outline outline-2 outline-neutral-300 dark:outline-neutral-600 ring-4 ring-inset ring-neutral-50 dark:ring-neutral-800 bg-neutral-200 dark:bg-neutral-700"
     >
         <div class="flex flex-col gap-4">
             <p
-                class="font-bold font-[Montserrat] text-5xl text-neutral-900 dark:text-neutral-100"
+                class="font-bold font-[Montserrat] text-5xl text-neutral-800 dark:text-neutral-100"
             >
-                Welcome to Avatio
+                Welcome to <span class="font-black">Avatio</span>
             </p>
             <p class="font-medium text-neutral-600 dark:text-neutral-300">
                 あなたのアバターを共有しよう
