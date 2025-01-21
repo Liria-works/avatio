f1423136211818569371244c424f17f22c459cba
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-01-21 17:48:36 +0900

    add : vercel analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
 
 <template>
+    <Analytics />
     <NuxtRouteAnnouncer />
     <NuxtLayout>
         <NuxtPage />
