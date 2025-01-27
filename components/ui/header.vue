95b1459100674169ba9b37cc8ef87d23359c02eb
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-01-27 16:52:45 +0900

    styling fix

--- components/ui/header.vue
+++ components/ui/header.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-    <header class="flex items-center justify-between w-full">
+    <header class="flex items-center gap-3 justify-between w-full">
         <div class="flex flex-wrap items-center gap-x-4 gap-y-2">
             <NuxtLink to="/" class="pt-1">
                 <LogoLiria class="w-32 sm:w-48 -mt-1" aria-label="Avatio" />
             </NuxtLink>
-            <BadgeHeader />
+            <BadgeHeader class="hidden sm:block" />
         </div>
         <UiHeaderMenu />
     </header>
