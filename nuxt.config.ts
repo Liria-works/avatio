8735875f8e58dc352a84e5c7baea5c7d946592d5
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-01-27 16:26:35 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,9 @@ const supabase = createClient(
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    future: {
+        compatibilityVersion: 4,
+    },
     compatibilityDate: '2024-08-21',
     devtools: {
         enabled: true,
