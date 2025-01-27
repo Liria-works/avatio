bd3b06dbaeff356b31bef7b5cc5ce1868e50a8dd
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-01-27 16:27:11 +0900

    dev : maintenance mode

---
+++ components/ui/isMaintenance.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const isMaintenance = ref<boolean>(false);
+
+onMounted(async () => {
+    const res = await $fetch('/api/edgeConfig');
+    if (res.value) isMaintenance.value = res.value.is_maintenance as boolean;
+});
+</script>
+
+<template>
+    <div v-if="isMaintenance" class="leading-none">Maintenance now</div>
+</template>
