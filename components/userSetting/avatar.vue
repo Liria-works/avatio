c718637b19c0f8af2be999be7aa3888b361ea6c0
Liry24 <liry241999@gmail.com>
2025-01-27 18:48:53 +0900

    fix : ogp

--- components/userSetting/avatar.vue
+++ components/userSetting/avatar.vue
@@ -2,15 +2,7 @@
 const image = defineModel<{ oldName: string | null; new: File | null }>({
     default: null,
 });
-// interface Props {
-//     initial: string;
-// }
-// const props = defineProps<Props>();
-// const avatar = ref<string>(props.initial);
 const imagePreview = ref<string | ArrayBuffer | null>(null);
-
-// const client = useSupabaseClient();
-// const user = useSupabaseUser();
 
 const loading = ref(false);
 
@@ -20,7 +12,7 @@ const { open, onChange } = useFileDialog({
 });
 
 onChange((files) => {
-    if (files?.length) {
+    if (files?.length && files[0]) {
         const file = files[0];
         image.value.new = file;
 
@@ -35,54 +27,6 @@ onChange((files) => {
         imagePreview.value = null;
     }
 });
-
-// const changeAvatar = async () => {
-//     const { open, onChange } = useFileDialog({
-//         accept: 'image/png, image/jpeg, image/webp, image/avif, image/gif, image/svg, image/tiff',
-//         multiple: false,
-//     });
-
-//     const faild = () => {
-//         useAddToast(
-//             'アバターの変更に失敗しました',
-//             '画像の形式が非対応の可能性があります。'
-//         );
-//         loading.value = false;
-//     };
-
-//     open();
-//     onChange(async (files) => {
-//         if (!files || files.length === 0) throw new Error('No files selected');
-//         const file = files[0];
-
-//         loading.value = true;
-
-//         const uploaded = await usePutImage(file, {
-//             resolution: 512,
-//             size: 300,
-//             prefix: 'avatar',
-//         });
-//         if (!uploaded) return faild();
-
-//         const { data: legacy } = await client
-//             .from('users')
-//             .select('avatar')
-//             .eq('id', user.value.id)
-//             .maybeSingle();
-
-//         const { error } = await client
-//             .from('users')
-//             .update({ avatar: uploaded.name } as never)
-//             .eq('id', user.value.id);
-//         if (error) return faild();
-
-//         if (legacy) await useDeleteImage(legacy.avatar, { prefix: 'avatar' });
-
-//         useAddToast('アバターを変更しました');
-//         avatar.value = uploaded.name;
-//         loading.value = false;
-//     });
-// };
 </script>
 
 <template>
--- composables/interface.ts
+++ composables/interface.ts
@@ -73,58 +73,21 @@ export const useOGP = ({
     image?: '/ogp.png' | string | null;
     twitterCard?: 'summary' | 'summary_large_image' | null;
 }) => {
-    const meta: object[] = [
-        {
-            hid: 'og:url',
-            property: 'og:url',
-            content: url ?? useBrowserLocation().value.href!,
-        },
-        {
-            hid: 'og:title',
-            property: 'og:title',
-            content: title,
-        },
-        {
-            hid: 'twitter:title',
-            property: 'twitter:title',
-            content: title,
-        },
-        {
-            hid: 'og:image',
-            property: 'og:image',
-            content: image ?? '/ogp.png',
-        },
-        {
-            hid: 'twitter:image',
-            property: 'twitter:image',
-            content: image ?? '/ogp.png',
-        },
-        { name: 'twitter:card', content: twitterCard ?? 'summary' },
-        { name: 'twitter:site', content: '@liria_work' },
-        { hid: 'og:type', property: 'og:type', content: type ?? 'article' },
-    ];
-
-    if (description && description.length) {
-        meta.push({
-            hid: 'description',
-            name: 'description',
-            content: description,
-        });
-        meta.push({
-            hid: 'og:description',
-            property: 'og:description',
-            content: description,
-        });
-        meta.push({
-            hid: 'twitter:description',
-            property: 'twitter:description',
-            content: description,
-        });
-    }
-
-    return useHead({
+    return useSeoMeta({
+        ogType: type ?? 'article',
+        ogUrl: url ?? useBrowserLocation().value.href!,
         title: title,
         titleTemplate: titleTemplate ?? '%s | Avatio',
-        meta: meta,
+        ogTitle: title,
+        twitterTitle: title,
+        description: description,
+        ogDescription: description,
+        twitterDescription: description,
+        ogImage: image ?? '/ogp.png',
+        twitterImage: image ?? '/ogp.png',
+        ogImageAlt: title,
+        twitterImageAlt: title,
+        twitterCard: twitterCard ?? 'summary',
+        twitterSite: 'liria_work',
     });
 };
