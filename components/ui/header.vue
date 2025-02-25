4c3cdf430f5e7e790067f60e31228fc38ccb4f7b
Liry24 <47878070+Liry24@users.noreply.github.com>
2025-02-25 17:20:01 +0900

    dev : header banner
    - ヘッダーにお知らせバナーを追加
    - 従来のバッジは削除

--- components/ui/header.vue
+++ components/ui/header.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-    <header class="flex items-center gap-3 justify-between w-full">
-        <div class="flex flex-wrap items-center gap-x-4 gap-y-2">
+    <header class="flex items-center gap-6 justify-between w-full">
+        <div class="w-full flex items-center gap-x-4 gap-y-2">
             <NuxtLink to="/" class="pt-1">
                 <LogoLiria class="w-24 sm:w-32" aria-label="Avatio" />
             </NuxtLink>
-            <BadgeHeader class="hidden sm:block" />
+
+            <BannerHeader />
         </div>
         <UiHeaderMenu />
     </header>
